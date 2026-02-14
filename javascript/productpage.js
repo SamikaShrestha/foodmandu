7653375e0e364580d91ed443e7ee479075aaa5df
SamikaShrestha <shresthasamika640@gmail.com>
2026-02-14 11:25:57 +0545

    Fixed button

---
+++ javascript/productpage.js
@@ -0,0 +1,8 @@
+// const addButtons = document.querySelectorAll('.addtobag');
+// const bagText = document.querySelector('.bag-text');
+
+// addButtons.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         bagText.textContent = "You have items in your bag!";
+//     });
+// });
