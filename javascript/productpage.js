3fb43c3a5d51ded2f6b561df74b0200b4f324cb5
SamikaShrestha <shresthasamika640@gmail.com>
2026-03-17 20:03:00 +0545

    added a menu dropdown

--- javascript/productpage.js
+++ javascript/productpage.js
@@ -1,10 +1,20 @@
-// Sticky Header
-window.addEventListener("scroll", () => {
-  const header = document.querySelector("header");
-  header.classList.toggle("sticky", window.scrollY > 50);
-});
 
 // Add to Cart
 function addToCart(pizzaName) {
     alert(pizzaName + " added to cart!");
 }
+
+const dropdown = document.querySelector(".custom-dropdown");
+const button = document.querySelector(".dropdown-btn");
+const items = document.querySelectorAll(".dropdown-list li");
+
+button.addEventListener("click", () => {
+    dropdown.classList.toggle("active");
+});
+
+items.forEach(item => {
+    item.addEventListener("click", () => {
+        button.textContent = item.textContent;
+        dropdown.classList.remove("active");
+    });
+});
